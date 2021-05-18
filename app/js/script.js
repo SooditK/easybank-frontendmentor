d5b752784bf184290ef8ef17b42e2503b10ebf05
SooditK <sooditkumarabc@gmail.com>
2021-05-18 09:53:35 +0530

    Project Completed

--- app/js/script.js
+++ app/js/script.js
@@ -4,7 +4,7 @@ const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has-fade");
 btnHamburger.addEventListener("click", function () {
-  console.log("open hamburger");
+  console.log("click hamburger");
   if (header.classList.contains("open")) {
     body.classList.remove("noscroll");
     header.classList.remove("open");
@@ -16,8 +16,8 @@ btnHamburger.addEventListener("click", function () {
     body.classList.add("noscroll");
     header.classList.add("open");
     fadeElems.forEach(function (element) {
-      element.classList.add("fade-in");
       element.classList.remove("fade-out");
+      element.classList.add("fade-in");
     });
   }
 });
